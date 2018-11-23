499211c1e8697bb17c4962696c64565355aa538c
Pablo Moreno <pcm32@users.noreply.github.com>
2018-11-23 10:13:48 +0000

    Open targets exporter (#3)
    * Open target exports scripts
    
    * Fixes query
    
    * Avoid database connection for open targets
    
    * Fix call to ot_json_schema_validator.pl
    
    * Make curl silent for less logging
    
    * Uses python validator for OT
    
    * Make virtualenv only if it is not there.
    
    * Improve pip installations
    
    * Move error handling to after the validation install.
    
    * Bash error handling improved around virtualenv
    
    * Fix the way that the validator is called.
    
    * Remove stderr/stdout redefines
    
    * Change strategy as python validator doesn't output json
    
    * Check errors based on error file.

---
+++ bin/export_open_targets.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#defaults
+atlasUrl=${atlasUrl:-"https://wwwdev.ebi.ac.uk/gxa"}
+urlParams=${urlParams:-"logFoldChangeCutoff=1.0&pValueCutoff=0.05&maxGenesPerContrast=1000"}
+destination=${destination:-"$ATLAS_FTP/experiments/cttv010-$(date "+%Y-%m-%d").json"}
+usageMessage="Usage: (-a $atlasUrl) (-p urlParams:$urlParams) (-d destination:$destination) (-o outputPath:outputPath)"
+venvPath=${venvPath:-"$ATLAS_PROD/venvs"}
+
+echo "To exclude experiments for open-targets, export env var EXPERIMENTS_TO_EXCLUDE=ACC1;...;ACCi;..ACCn"
+
+while getopts ":a:p:d:o:" opt; do
+  case $opt in
+    a)
+      atlasUrl=$OPTARG;
+      ;;
+    p)
+      urlParams=$OPTARG;
+      ;;
+    d)
+      destination=$OPTARG;
+      ;;
+    o)
+      outputPath=$OPTARG;
+      ;;
+    ?)
+      echo "Unknown option: $OPTARG"
+      echo $usageMessage
+      exit 2
+      ;;
+  esac
+done
+
+listExperimentsToRetrieve(){
+    IFS='; ' read -r -a exclude_exp <<< "$EXPERIMENTS_TO_EXCLUDE"
+    printf "%s\n" "${exclude_exp[@]}" > experiments-exclude.tmp
+    comm -23 \
+      <( curl -s $atlasUrl/json/experiments | jq -c -r '.aaData | map(select(.species=="Homo sapiens")) | map(select(.experimentType | test("(MICROARRAY)|(DIFFERENTIAL)"; "i")) |.experimentAccession) | @csv' | tr -s ',' '\n' | sed 's/"//g' \
+        | sort -u ) \
+      <( cut -f1 -d ' ' "experiments-exclude.tmp" | sort)
+}
+
+installValidator(){
+  if [ ! -f $venvPath/ot-validator/bin/activate ]; then
+    mkdir -p $venvPath
+    virtualenv $venvPath/ot-validator
+  fi
+  source $venvPath/ot-validator/bin/activate
+  pip install --upgrade pip==18.1
+  pip install --upgrade setuptools==40.6.2
+  pip install opentargets-validator==0.3.0
+  deactivate
+}
+
+installValidator
+source $venvPath/ot-validator/bin/activate
+
+
+rm -rf ${destination}.tmp
+touch ${destination}.tmp
+
+
+trap 'mv -fv ${destination}.tmp ${destination}.failed; exit 1' INT TERM EXIT
+
+listExperimentsToRetrieve | while read -r experimentAccession ; do
+  echo "Retrieving experiment $experimentAccession ... "
+  curl -s -w "\n" "$atlasUrl/json/experiments/$experimentAccession/evidence?$urlParams" | grep -v -e '^[[:space:]]*$' > $experimentAccession.tmp.json
+  opentargets_validator --schema https://raw.githubusercontent.com/opentargets/json_schema/1.3.0/src/expression.json $experimentAccession.tmp.json 2>$experimentAccession.err
+  if [ $(wc -l < $experimentAccession.err) -eq 0 ]; then
+    cat $experimentAccession.tmp.json >> ${destination}.tmp
+    rm $experimentAccession.tmp.json
+    rm $experimentAccession.err
+  else
+    echo "$experimentAccession failed validation."
+    cat $experimentAccession.err
+    exit 1
+  fi
+done
+rm -rf experiments-exclude.tmp
+
+# closes virtualenv
+deactivate
+
+trap - INT TERM EXIT
+
+
+echo "Successfully fetched and validated evidence, zipping..."
+mv -nv ${destination}.tmp $destination
+gzip $destination
+
+echo "Sanity check .."
+"$scriptDir/ot_json_queries_stats.sh" -j ${destination}.gz -o $outputPath
---
+++ bin/ot_json_queries_stats.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -euo pipefail
+
+scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+usageMessage="Usage: (-j cttv010-2018-09-25.json.gz) (-o outputPath)"
+
+while getopts ":j:o:" opt; do
+  case $opt in
+    j)
+      jsonDump=$OPTARG;
+      ;;
+    o)
+      outputPath=$OPTARG;
+      ;;
+    ?)
+      echo "Unknown option: $OPTARG"
+      echo $usageMessage
+      exit 2
+      ;;
+  esac
+done
+
+echo "Json file: $jsonDump"
+echo "Output path: $outputPath"
+
+file_name=$(basename $jsonDump | sed 's/[.].*//')
+
+json_dump_stats(){
+	json_file=$1
+    outputPath=$2
+
+	file_name=$(basename $json_file | sed 's/[.].*//')
+
+ 	# retrieve evidences
+ 	zless $json_file | jq '.evidence.unique_experiment_reference' | sort  > $outputPath/${file_name}_experiments.txt
+
+ 	# number of microarray that has probe id field.
+ 	zless $json_file | jq '.unique_association_fields | select( has("probe_id"))' | grep "study_id"  \
+  		| grep  -oe 'E-[[:upper:]]*-[[:digit:]]*' | sort -u > $outputPath/${file_name}_micoarray.txt
+
+ 	# log fold change of each evidence.
+ 	zless $json_file | jq '.evidence.log2_fold_change.value' >  $outputPath/${file_name}_log_fold_changes.txt
+
+ 	# pvalues of each evidence.
+ 	zless $json_file | jq '.evidence.resource_score.value' >  $outputPath/${file_name}_pvalue.txt
+
+ 	# tabulate contrast experiments and genes associated with each study.
+	zless $json_file | jq '.unique_association_fields | .comparison_name + "  " + .study_id + "  " + .geneID' > $outputPath/${file_name}_contrast_exp_gene.txt
+
+	file_contrast=$outputPath/${file_name}_contrast_exp_gene.txt
+	exp=$(cat $file_contrast | awk -F"  " '{print $2}' | grep  -oe 'E-[[:upper:]]*-[[:digit:]]*' | sort -u)
+	for expAcc in $exp; do
+   		contrast=$(cat $file_contrast | grep "$expAcc" | awk -F"  " '{print $1}' | sort -u)
+      		for cont in $(echo -e $contrast | tr "\"" "\n" | sed '/^$/d'); do
+        			ngenes=$(cat $file_contrast | grep "$expAcc" | grep -F "$cont" | wc -l)
+         			echo -e "$expAcc\t$cont\t$ngenes"
+      		done
+	done
+}
+
+IFS="
+"
+
+echo "Running json dump stats .."
+json_dump_stats $jsonDump $outputPath > $outputPath/${file_name}_report.txt
+
+echo "sorting report .."
+cat $outputPath/${file_name}_report.txt | sort -t$'\t' -k3 -nr > $outputPath/${file_name}_report_sort.txt
+
+echo "Plots for sanity .."
+"$scriptDir/ot_json_evidence_plots.R" -i $outputPath/${file_name}_experiments.txt -l $outputPath/${file_name}_log_fold_changes.txt -p $outputPath/${file_name}_pvalue.txt -o $outputPath
