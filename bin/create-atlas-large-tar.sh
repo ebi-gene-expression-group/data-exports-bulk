0d82d459f1b2415442ec9d1cb10aef7567557bd5
Suhaib Mohammed <suhaib@ebi.ac.uk>
2019-11-06 16:53:29 +0000

    Feature/tarball fix regular exp (#9)
    * regular expression improvement to get rid of spaces
    
    * compare file logic changed to identify that unique in release that is not in staging
    
    * remove reprocessed studies and reload in tarball

--- bin/create-atlas-large-tar.sh
+++ bin/create-atlas-large-tar.sh
@@ -31,23 +31,28 @@ gunzip atlas-latest-data.tar.gz
 # Identify all experiments removed since $lastReleaseDate
 tar -tvf atlas-latest-data.tar | grep '/$' | sed 's|\/$||' | awk '{print $NF}' | sort | uniq > $aux.exps_in_release_$lastReleaseDate
 ls -la | grep E- | egrep -v 'rwxr-x-' | awk '{print $NF}' | sort > $aux.all_current_experiments
-comm -3 $aux.exps_in_release_$lastReleaseDate $aux.all_current_experiments | grep '^E-'> $aux.exps_removed_since_last_release
+## find common studies in tarball and recently loaded studies. The common ones needs to removed and loaded again as they are reprocessed
+comm -12 $aux.exps_in_release_$lastReleaseDate $aux.exps_loaded_since_$lastReleaseDate | grep -oe 'E-[[:upper:]]*-[[:digit:]]*' > $aux.exps_removed_since_last_release
 # First remove from atlas-latest-data.tar all experiments in $aux.exps_removed_since_last_release
-for e in $(cat $aux.exps_removed_since_last_release); do
-    echo "Removing $e ..."
-    tar --delete $e -f atlas-latest-data.tar
-    if [ $? -ne 0 ]; then
-        echo "ERROR: removing $e from atlas-latest-data.tar"
-        exit 1
-    fi
-done
+if [ -s "$aux.exps_removed_since_last_release" ]; then
+    for e in $(cat $aux.exps_removed_since_last_release); do
+        echo "Removing $e ..."
+        tar --delete $e -f atlas-latest-data.tar
+        if [ $? -ne 0 ]; then
+            echo "ERROR: removing $e from atlas-latest-data.tar"
+            exit 1
+        fi
+    done
+fi
 # Now add/update in atlas-latest-data.tar all experiments in $aux.exps_loaded_since_$lastReleaseDate
-for e in $(cat $aux.exps_loaded_since_$lastReleaseDate); do
-    echo "Adding/updating $e ..."
-    tar -r $e --exclude "$e/qc" --exclude "$e/archive" -f atlas-latest-data.tar
-    if [ $? -ne 0 ]; then
-        echo "ERROR: adding/updating $e in atlas-latest-data.tar"
-        exit 1
-    fi
-done
+if [ -s "$aux.exps_loaded_since_$lastReleaseDate" ]; then
+    for e in $(cat $aux.exps_loaded_since_$lastReleaseDate); do
+        echo "Adding/updating $e ..."
+        tar -r $e --exclude "$e/qc" --exclude "$e/archive" -f atlas-latest-data.tar
+        if [ $? -ne 0 ]; then
+            echo "ERROR: adding/updating $e in atlas-latest-data.tar"
+            exit 1
+        fi
+    done
+fi    
 gzip atlas-latest-data.tar
