e9039b5c0c47573530358de42dc9f275f23ac185
Andrey Solovyev <a.solovyev39@gmail.com>
2021-03-08 17:13:10 +0000

    Remove tmp file

--- bin/run_schema_transform.sh
+++ bin/run_schema_transform.sh
@@ -5,10 +5,8 @@
 [ -z ${SCHEMA_TRANSFORM}+x ] && echo "Error: variable SCHEMA_TRANSFORM is not defined." && exit 1 
 
 touch /data/$PROCESSED_JSON
-touch /data/tmp_1.txt
 
 while read line; do
-    echo "LINE" >> /data/tmp_1.txt
     echo $line > tmp.json
     java -cp /app/run-jslt.jar com.schibsted.spt.data.jslt.cli.JSLT /data/$SCHEMA_TRANSFORM tmp.json >> /data/$PROCESSED_JSON
 done < /data/$INPUT_JSON
