cd9dc4015b093a0be4c3eff7a730167b87a5fff1
Iris Diana Yu <irisyu@ebi.ac.uk>
2023-08-03 16:01:29 +0100

    Add time stamp to export file name and link to generic file name

--- bin/export_atlas_uniprot.sh
+++ bin/export_atlas_uniprot.sh
@@ -12,8 +12,8 @@
 
 atlasUniqueUniprotUrl="http://${SOLR_HOST}/solr/bulk-analytics-v1/select?facet.field=keyword_uniprot&facet=on&q=*:*&rows=0&start=0"
 ATLAS_UNIPROT_EXPORT_FILE=${$1:-$ATLAS_UNIPROT_EXPORT_FILE}
-
-rm -f $ATLAS_UNIPROT_EXPORT_FILE
+today=$( eval date +%F_%Hh%Mm%Ss )
+datedExportFile="${ATLAS_UNIPROT_EXPORT_FILE}.${today}"
 
 # Get all unique UNIPROT IDs in bulk-analytics
 uniprot_ids=$(curl -u $SOLR_USER:$SOLR_PASS $atlasUniqueUniprotUrl | jq '.facet_counts.facet_fields["keyword_uniprot"]' | grep -Po '(?<=\")[a-z0-9]+')
@@ -21,5 +21,8 @@ uniprot_ids=$(curl -u $SOLR_USER:$SOLR_PASS $atlasUniqueUniprotUrl | jq '.facet_
 # Generate Atlas search links
 for uniprot_id in $uniprot_ids; do
     url="https://wwwdev.ebi.ac.uk/gxa/search?geneQuery=[{%22value%22:%22${uniprot_id}%22,%20%22category%22:%22uniprot%22}]"
-    echo -e "${uniprot_id}\t${url}" >> $ATLAS_UNIPROT_EXPORT_FILE
+    echo -e "${uniprot_id}\t${url}" >> $datedExportFile
 done
+
+rm -f $ATLAS_UNIPROT_EXPORT_FILE
+ln -s $ATLAS_UNIPROT_EXPORT_FILE $datedExportFile
