dda9f530ffbd695b9f81b28664f9c82e38a3e877
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2025-03-21 13:38:19 +0000

    Update create-ebeye-dump.sh

--- bin/create-ebeye-dump.sh
+++ bin/create-ebeye-dump.sh
@@ -5,19 +5,25 @@ scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 [ ! -z ${ATLAS_EXPS+x} ] || ( echo "Env var ATLAS_EXPS path to the experiment needs to be defined." && exit 1 )
 [ ! -z ${ATLAS_EBEYE+x} ] || ( echo "Env var ATLAS_EBEYE path to the ebeye dump directory needs to be defined." && exit 1 )
+[ ! -z ${ATLAS_PROD+x} ] || ( echo "Env var ATLAS_PROD path to the ebeye dump directory needs to be defined." && exit 1 )
 
 pushd $ATLAS_EBEYE
 
 
-## back up oldfiles
-today=`eval date +%d%b%Y`
+# backup old XML files
+today=$(date +%d%b%Y)
 mkdir -p archive/$today
 find . -name \*.xml -exec cp -p {} ./archive/$today \;
 
+# run export script
 $scriptDir/export_atlas_ebeye_xml.pl
 if [ $? -ne 0 ]; then
     echo "ERROR: Failed to generate EB-eye dump"
     exit 1
 fi
-cp ebeye*.xml $ATLAS_PROD/EBEYE_dumps
+
+# copy new XML files to production directory
+cp ebeye*.xml "$ATLAS_PROD/EBEYE_dumps"
+echo "EB-eye dump successfully updated."
+
 popd
